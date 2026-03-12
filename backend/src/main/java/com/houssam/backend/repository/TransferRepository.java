8d70ce09ad0bd589090336e25f2670c3645b765b
houssam <lambarahoussam@gmail.com>
2026-03-12 13:50:46 +0000

    configuraion H2

--- backend/src/main/java/com/houssam/backend/repository/TransferRepository.java
+++ backend/src/main/java/com/houssam/backend/repository/TransferRepository.java
@@ -3,5 +3,9 @@ package com.houssam.backend.repository;
 import com.houssam.backend.entity.Transfer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TransferRepository extends JpaRepository<Transfer, String> {
+
+    List<Transfer> findAllByOrderByCreatedAtDesc();
 }
