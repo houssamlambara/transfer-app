6404891b2b5dbde2d5339e8c93d40bf9e4485d46
houssam <lambarahoussam@gmail.com>
2026-03-12 13:45:30 +0000

    ajout d'annotation pour la createdate

--- backend/src/main/java/com/houssam/backend/entity/Transfer.java
+++ backend/src/main/java/com/houssam/backend/entity/Transfer.java
@@ -2,11 +2,12 @@ package com.houssam.backend.entity;
 
 import com.houssam.backend.enums.TransferStatus;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.PastOrPresent;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -17,10 +18,11 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "transfers")
+@EntityListeners(AuditingEntityListener.class)
 public class Transfer {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
 
     @Column(nullable = false)
@@ -32,12 +34,12 @@ public class Transfer {
     @Column(nullable = false)
     private BigDecimal amount;
 
+    @Builder.Default
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    private TransferStatus status;
+    private TransferStatus status = TransferStatus.PENDING;
 
+    @CreatedDate
     @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
-
 }
-
